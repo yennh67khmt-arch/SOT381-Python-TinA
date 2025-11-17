9c4ac5bc5a21783d672ecc63159096cd257590ca
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 06:52:40 -0800

    bt6vs7

---
+++ bt6.py
@@ -0,0 +1,7 @@
+# BÀI TẬP THỰC HÀNH 6
+
+x = int(input("nhập số giá trị của x: "))
+
+A = x^2*2 + 3*x -5
+
+print("giá trị của biểu thức A là: ", A )
---
+++ bt7.py
@@ -0,0 +1,10 @@
+# BÀI TẬP THỰC HÀNH 7
+
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+
+a, b = b, a 
+
+print("Sau khi hoán đổi:")
+print("a =", a)
+print("b =", b)
