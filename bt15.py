e2cd51bb6b811d48b05b6e30e66c7a01d89c8e4b
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:45:28 -0800

    Create bt15.py

---
+++ bt15.py
@@ -0,0 +1,9 @@
+# BÀI TẬP THỰC HÀNH 15
+
+toan = float(input("nhập điểm môn toán: "))
+ly = float(input("nhập điểm môn lý: "))
+hoa = float(input("nhập điểm môn hóa: "))
+
+diem_trung_binh = (toan + ly + hoa) / 3
+
+print("điểm trung bình 3 môn là: ", diem_trung_binh)
