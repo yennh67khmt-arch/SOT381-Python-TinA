25e6cef5d0938c8b6ba577b74edf690560d36182
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 06:37:01 -0800

    Create bt4.py

---
+++ bt4.py
@@ -0,0 +1,8 @@
+# BÀI TẬP THỰC HÀNH 4
+
+can_nang = int(input("nhập chỉ số cân nặng: "))
+chieu_cao = int(input("nhập chỉ số chiều cao: "))
+
+BMI = can_nang / ( chieu_cao * chieu_cao )
+
+print("chỉ số BMI của bạn là: ", BMI)
