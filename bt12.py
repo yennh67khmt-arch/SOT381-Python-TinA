6f00c2b7c1eb17bb19afe65b766253c5824f1e20
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:24:35 -0800

    Create bt12.py

---
+++ bt12.py
@@ -0,0 +1,8 @@
+# BÀI TẬP THỰC HÀNH 12
+
+so = float(input("nhập số bất kỳ: "))
+phan_tram = float(input("nhập số phần trăm: "))
+
+ket_qua = so * phan_tram / 100
+
+print("kết quả phần trăm của số là: ", ket_qua)
