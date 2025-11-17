f23a1cbc157110cf20537c4d5612fcfa064520f5
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:49:44 -0800

    Create bt16.py

---
+++ bt16.py
@@ -0,0 +1,10 @@
+# BÀI TẬP THỰC HÀNH 16
+
+pi = 3.14159
+r = float(input("nhập bán kính hình tròn: "))
+
+chu_vi = 2 * pi * r
+dien_tich = pi * r * r
+
+print("chu vi hình tròn là: ", chu_vi)
+print("diện tích hình tròn là: ", dien_tich)
