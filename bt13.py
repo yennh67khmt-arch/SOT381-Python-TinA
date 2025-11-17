833209e30fc67c4c1628ec6ffc1bf74759e7b8ab
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:38:37 -0800

    Create bt13.py

---
+++ bt13.py
@@ -0,0 +1,10 @@
+# BÀI TẬP THỰC HÀNH 13
+
+a = int(input("nhập số nguyên a: "))
+b = int(input("nhập số nguyên b: "))
+        
+phan_nguyen = a // b
+phan_du = a % b
+        
+print("phần nguyên= ", phan_nguyen)
+print("phần dư= ", phan_du)        
