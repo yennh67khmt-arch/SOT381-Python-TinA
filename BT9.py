810680d41c43862c4a3ee0d55462d3013991789e
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 06:59:44 -0800

    Create BT9.py

---
+++ BT9.py
@@ -0,0 +1,9 @@
+# BÀI TẬP THỰC HÀNH 9
+
+gio = int(input("Nhập số giờ: "))
+phut = int(input("Nhập số phút: "))
+giay = int(input("Nhập số giây: "))
+
+tong_giay = gio * 3600 + phut * 60 + giay
+
+print("Tổng số giây là: ", tong_giay)
