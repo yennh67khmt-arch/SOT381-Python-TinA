d97d07835aa96d8d97c2d3a279451ab6ddd2d9ef
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 06:22:02 -0800

    Create bt1.py

---
+++ bt1.py
@@ -0,0 +1,10 @@
+# Bài 1: Tính tổng và tích hai số
+
+a = int(input("Nhập số thứ nhất: "))
+b = int(input("Nhập số thứ hai: "))
+
+tong = a + b
+tich = a * b
+
+print("Tổng =", tong)
+print("Tích =", tich)
