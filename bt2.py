7bac16e5a13b09ef3c4237c30f47f85863b2c0ba
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 06:30:55 -0800

    Create bt2.py

---
+++ bt2.py
@@ -0,0 +1,10 @@
+# BÀI TẬP THỰC HÀNH 2
+
+dai = int(input("nhập chiều dài hình chữ nhật: "))
+rong = int(input("nhập chiều rộng hình chữ nhật: "))
+
+chu_vi = (dai + rong) * 2
+dien_tich = dai * rong
+
+print("chu vi hình chữ nhật là: ", chu_vi)
+print("diện tích hình chữ nhật là: ", dien_tich)
