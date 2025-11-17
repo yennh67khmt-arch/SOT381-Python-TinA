ab56ac9a4b4e4d534cd2ea094b507dc36068aa0e
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 08:08:23 -0800

    Create bt20.py

---
+++ bt20.py
@@ -0,0 +1,20 @@
+# BÀI TẬP THỰC HÀNH 20
+
+a = int(input("nhập số nguyên a bất kỳ: "))
+b = int(input("nhập số nguyên b bất kỳ: "))
+
+tong = a + b
+tich = a * b
+hieu = a - b
+thuong = a / b
+luy_thua = a^b
+phan_nguyen = a // b
+phan_du = a % b
+
+print("tổng= ", tong)
+print("tích= ", tich)
+print("hiệu= ",hieu)
+print("thương= ", thuong)
+print("lũy thừa= ", luy_thua)
+print("phần nguyên= ", phan_nguyen)
+print("phần dư= ", phan_du)
