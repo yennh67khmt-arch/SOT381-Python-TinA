ecef77a511b2ca3ea62340084dd2d6c166ca4241
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:42:39 -0800

    Create bt14.py

---
+++ bt14.py
@@ -0,0 +1,8 @@
+# BÀI TẬP THỰC HÀNH 14
+
+quang_duong = float(input("nhập quãng đường: "))
+thoi_gian = float(input("nhập thời gian: "))
+
+van_toc = quang_duong / thoi_gian
+
+print("vận tốc= ", van_toc)
