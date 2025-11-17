80e5fe5c640bfafbb8e2e34855032793c3d04f43
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:54:11 -0800

    Create bt17.py

---
+++ bt17.py
@@ -0,0 +1,9 @@
+# BÀI TẬP THỰC HÀNH 17
+
+don_hang = float(input("nhập giá trị đơn hàng: "))
+
+giam_gia = don_hang * 15 /100
+tien_giam = don_hang - giam_gia
+
+print("số tiền dduuwwocj giảm = ", giam_gia)
+print(" số tiền cần trả = ", tien_giam)
