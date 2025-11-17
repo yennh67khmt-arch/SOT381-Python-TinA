0b5e508388d2784162aa1b99508f3b6a4dcfab67
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:59:44 -0800

    Create bt18.py

---
+++ bt18.py
@@ -0,0 +1,8 @@
+# BÀI TẬP THỰC HÀNH 18
+
+so_gio = float(input("nhập số giờ làm việc trong ngày: "))
+so_ngay = float(input("nhập số ngày làm việc trong tháng: "))
+
+tong = so_gio * so_ngay
+
+print("tổng số giờ làm việc là: ", tong)
