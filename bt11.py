604b8715cc39b674b9c2e7163d36923d020472e9
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-11-17 07:19:24 -0800

    Create bt11.py

---
+++ bt11.py
@@ -0,0 +1,7 @@
+# BÀI TẬP THỰC HÀNH 11
+
+so_tuoi = int(input("nhập số tuổi của bạn: "))
+ten = input("nhập tên của bạn: ")
+que_quan = input("nhập quê quán của bạn: ")
+
+print("xin chào, tôi tên là {ten}, {so_tuoi} tuổi, quê ở {que_quan}.")
