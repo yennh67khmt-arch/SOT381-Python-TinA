73fb1743fc16873b90a0af14497762fdb93f8cbc
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-12-22 14:05:05 -0800

    Update th2212_bai1.py

--- th2212_bai1.py
+++ th2212_bai1.py
@@ -1,5 +1,5 @@
-d = int(input("nhập chiều rộng hcn:"))
-r = int(input("nhập chiều dài hcn:"))
+d = float(input("nhập chiều rộng hcn:"))
+r = float(input("nhập chiều dài hcn:"))
         
 c = (d + r)*2
 s = d*r
