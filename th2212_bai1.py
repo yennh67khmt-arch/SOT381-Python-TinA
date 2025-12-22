6f973d77400b75d932511cceec47728d738192dd
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-12-22 14:26:36 -0800

    Update th2212_bai1.py

--- th2212_bai1.py
+++ th2212_bai1.py
@@ -1,8 +1,12 @@
 d = float(input("nhập chiều rộng hcn:"))
 r = float(input("nhập chiều dài hcn:"))
-while d>=0.0 and r>=0.0:       
- c = (d + r)*2
- s = d*r
- break   
+while True:
+    if (d>=0.0) and (r>=0.0):       
+       break
+    else:
+        print("yêu cầu nhập lại:")
+    
+c = (d + r)*2
+s = d*r  
 print(f"chu vi hcn là {c:.2f}")
 print(f"diện tích hcn là {s:.2f}")
