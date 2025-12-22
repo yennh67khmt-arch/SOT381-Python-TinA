38a963c2eaf9dac27e1c1888c745f69787ba1d50
yennh67khmt-arch <yen.nh.67khmt@ntu.edu.vn>
2025-12-22 14:02:56 -0800

    Create th2212_bai1.py

---
+++ th2212_bai1.py
@@ -0,0 +1,8 @@
+d = int(input("nhập chiều rộng hcn:"))
+r = int(input("nhập chiều dài hcn:"))
+        
+c = (d + r)*2
+s = d*r
+        
+print(f"chu vi hcn là {c:.2f}")
+print(f"diện tích hcn là {s:.2f}")
